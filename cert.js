36c39de4eb7bae68b1b59d4f6231f161b9b6dd43
AlexNett <AN@gmail.com>
2019-05-11 18:59:35 +0200

    modal

--- cert.js
+++ cert.js
@@ -1,7 +1,10 @@
 var link = document.getElementById("Download");
+var popup = document.getElementById("popup");
 var body = document.querySelectorAll("BODY")[0];
 var html = document.querySelectorAll("HTML")[0];
 var text = document.getElementById("OS");
+var modal = document.getElementById("MODAL");
+var screen = document.getElementById("SCREEN");
 
 text.append( document.createTextNode("[" + getOS() + "]") );
 
@@ -22,4 +25,37 @@ function certDotDer(){
 
 function certDotPem(){
     window.location='cert.pem';
+}
+
+/* Modal */
+
+popup.onclick = showModal;
+screen.onclick = closeModal;
+body.style.transition = "0.2s";
+screen.style.display = "block";
+modal.style.display = "block";
+
+function showModal(){
+
+    body.style.filter = "blur(5px)";
+    ModalOpen();
+}
+
+function closeModal(){
+
+    body.style.filter = "blur(0px)";
+    ModalClose();
+}
+
+ModalClose();
+
+function ModalOpen(){
+    modal.className = "up";
+    body.append(screen);
+    html.append(modal);
+}
+
+function ModalClose(){
+    modal.remove();
+    screen.remove();
 }
