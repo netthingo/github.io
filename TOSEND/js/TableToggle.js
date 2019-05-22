2a47533a8f6bfb538f6e613cb25088b2340c335c
AlexNett <AN@gmail.com>
2019-05-22 18:16:16 +0200

    tosend-list

---
+++ TOSEND/js/TableToggle.js
@@ -0,0 +1,51 @@
+var RowAmmountClosed = 3;
+var sleepTime = 10;
+
+var toggle = document.getElementById("toggle");
+var table = document.getElementById("INFO");
+var trs = table.getElementsByTagName("tr");
+var toggler = document.getElementsByClassName("fa-chevron-down");
+toggler[0].style.transition = "0.5s";
+
+for(var i = RowAmmountClosed; i < trs.length; i++){
+		trs[i].style.display = "none";
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+var deg = 180;
+
+async function close() {
+	
+	
+	deg -= 180;
+	
+	toggler[0].style.transform = "rotate("+ deg +"deg)";
+	for(var i = trs.length-(RowAmmountClosed-2); i > RowAmmountClosed-1; i--){
+		trs[i].style.display = "none";
+		
+  await sleep(sleepTime);
+	}
+	
+	toggle.onclick = open;
+	
+}
+
+async function open() {
+	
+	deg -= 180;
+	
+	toggler[0].style.transform = "rotate("+ deg +"deg)";
+	for(var i = RowAmmountClosed-1; i < trs.length; i++){
+		trs[i].style.display = "table-row";
+  await sleep(sleepTime);
+	}
+	
+	toggle.onclick = close;
+	
+}
+
+toggle.onclick =  close;
+close();
